b427e34ed0b835359ef9a29ce38339e3be0d7d3b
yzdalhj <yzdalhj@163.com>
2018-04-17 14:40:21 +0800

    test

---
+++ home/src/main/java/com/yzdahj/home/HomeApplication.java
@@ -0,0 +1,12 @@
+package com.yzdahj.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeApplication.class, args);
+	}
+}
